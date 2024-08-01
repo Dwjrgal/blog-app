b5a99244d9a82f9ec0a037bc57b8b4e219f17ef4
dwjrgal <dwjrgl651@gmail.com>
2024-08-01 23:49:07 +0800

    trending cards

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from './slider';
+import Trending from './trending';
 
 const content = () => {
   return (
     <div>
         <Slider/>
+        <Trending/>
     </div>
   )
 }
--- src/components/content/trending.jsx
+++ src/components/content/trending.jsx
@@ -1,22 +1,48 @@
 import React from 'react'
 
-const TrendingCards ={
+const cards = [
+  {
     title: "technology",
     text: "The Impact of Technology on the Workplace: How Technology is Changing",
-    img: image,
+    img: "https://plus.unsplash.com/premium_photo-1721980274417-8d3d99e69ba0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "technology",
+    text: "The Impact of Technology on the Workplace: How Technology is Changing",
+    img: "https://plus.unsplash.com/premium_photo-1721980274417-8d3d99e69ba0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "technology",
+    text: "The Impact of Technology on the Workplace: How Technology is Changing",
+    img: "https://plus.unsplash.com/premium_photo-1721980274417-8d3d99e69ba0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "technology",
+    text: "The Impact of Technology on the Workplace: How Technology is Changing",
+    img: "https://plus.unsplash.com/premium_photo-1721980274417-8d3d99e69ba0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 
 
-}
+
+]
 
 const Trending = () => {
   return (
     <div>
         <h2>Trending</h2>
-        <div>
-            
-        </div>
+        
+            {cards.map((card) => (
+                <div className=''>
+                <h4> {cards.title}</h4>
+                <p> {cards.text} </p>
+                <img 
+                src={cards.img}
+                />
+                </div>
+
+            ))}
     </div>
-  )
-}
+  );
+};
 
 export default Trending
