8e7b0263f3baf85aba90841253cbfeb5144cbd36
dwjrgal <dwjrgl651@gmail.com>
2024-08-09 10:09:37 +0800

    some changes

--- src/components/footer/links.jsx
+++ src/components/footer/links.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Links = () => {
   return (
     <div className="md:flex md:justify-between my-16 border-t-2 pt-6 border-zinc-300">
-      <img className="h-[35px] w-[80px]" src="./images/Logo.png" alt="" />
+      <img className="h-8" src="./images/Copyright Info.svg" alt="" />
       <ul className="md:flex gap-5 hidden md:text-sm">
         <li>Terms of Use</li>
         <li>Privacy Policy</li>
--- src/components/header/logo.jsx
+++ src/components/header/logo.jsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <img
       className=" h-6 w-20 md:w-[130px] md:h-[25px] m-5"
-      src="./images/Logo.png"
+      src="./images/Logo.svg"
       alt=""
     />
   );
