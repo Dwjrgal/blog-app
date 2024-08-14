0fd1d6a2774e9b0e9585da28a5c6ca43ae9b29e9
dwjrgal <dwjrgl651@gmail.com>
2024-08-14 09:10:13 +0800

    blog page justify-center

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -116,39 +116,41 @@ const Blog = () => {
 
   return (
     <>
-      <Link href={"/blog/" + articles.id}>
-        <div className="mt-20 flex flex-col gap-6 mx-20">
-          <h3 className="text-md sm:text-lg font-bold">All Blog Post</h3>
-          <section className="md:grid md:grid-cols-3 sm:gap-4  gap-2 md:items-center">
-            {findPost.map((card) => (
-              <div className="sm:w-[280px] sm:h-[340px] border rounded flex flex-col gap-3 justify-center items-start pl-4">
-                <img
-                  className="sm:h-[160px] sm:w-[240px] rounded-md"
-                  src={card.social_image}
-                  alt=""
-                />
-                <span className="text-[11px] h-5 w-24  text-blue rounded-md text-center bg-slate-200 text-indigo-600 font-extralight">
-                  {card.type_of}
-                </span>
-                <p className=" sm:h-[80px] sm:w-[230px]  sm:text-[14x] sm:font-semibold  sm:font-base">
-                  {card.title}
-                </p>
-                <p className="text-[11px] font-extralight text-gray-600">
-                  {card.published_at}
-                </p>
-              </div>
-            ))}
-          </section>
-          <div className="flex justify-center">
-            <button
-              onClick={() => setCount(count + 3)}
-              className=" h-10 w-24 border rounded-md text-xs text-center font-extralight text-gray-500  my-10 hover:border-emerald-300"
-            >
-              Load More
-            </button>
+      <section className="flex justify-center">
+        <Link href={"/blog/" + articles.id}>
+          <div className="mt-20 flex flex-col gap-6 mx-20">
+            <h3 className="text-md sm:text-lg font-bold">All Blog Post</h3>
+            <section className="md:grid md:grid-cols-3 sm:gap-6  gap-2 md:items-center">
+              {findPost.map((card) => (
+                <div className="sm:w-[280px] sm:h-[340px] border rounded flex flex-col gap-3 justify-center items-start pl-4">
+                  <img
+                    className="sm:h-[160px] sm:w-[240px] rounded-md"
+                    src={card.social_image}
+                    alt=""
+                  />
+                  <span className="text-[11px] h-5 w-24  text-blue rounded-md text-center bg-slate-200 text-indigo-600 font-extralight">
+                    {card.type_of}
+                  </span>
+                  <p className=" sm:h-[80px] sm:w-[230px]  sm:text-[14x] sm:font-semibold  sm:font-base">
+                    {card.title}
+                  </p>
+                  <p className="text-[11px] font-extralight text-gray-600">
+                    {card.published_at}
+                  </p>
+                </div>
+              ))}
+            </section>
+            <div className="flex justify-center">
+              <button
+                onClick={() => setCount(count + 3)}
+                className=" h-10 w-24 border rounded-md text-xs text-center font-extralight text-gray-500  my-10 hover:border-emerald-300"
+              >
+                Load More
+              </button>
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </section>
     </>
   );
 };
