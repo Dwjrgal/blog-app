d22991b0c167e71c4b38aa53b6545e0e2fb3a986
dwjrgal <dwjrgl651@gmail.com>
2024-08-16 10:05:39 +0800

    footer fixed

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,7 +6,7 @@ import About from "./about";
 const Footer = () => {
   return (
     <div className="flex flex-col px-40 bg-[#E8E8EA] pt-10">
-      <div className="flex gap-20 items-center justify-center">
+      <div className="flex gap-20 items-center mx-10">
         <About />
       </div>
       <Links />
